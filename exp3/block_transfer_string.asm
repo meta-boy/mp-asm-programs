0ccb15bf32e4a1a2ecec223d402df1c78b3729c3
Anurag Patil <me@metaboy.info>
2020-11-02 12:39:41 +0530

    updated exp3

--- exp3/block_transfer_string.asm
+++ exp3/block_transfer_string.asm
@@ -1,29 +1,30 @@
-; Assembly language program to transfer data block without using string instruction
+; Assembly language program to transfer data block using string instruction
 
 .MODEL SMALL
 
 DATA SEGMENT
 X DB 10H, 20H, 30H, 40H, 50H
 Y DB ?
-DATA ENDS
-
+ENDS
+ 
 CODE SEGMENT
 ASSUME CS:CODE, DS:DATA
+
 START:
 	MOV AX, DATA
 	MOV DS, AX
+	MOV ES, AX
 	LEA SI, X
 	LEA DI, Y
-	MOV CX, 05H
+        MOV CX, 05H
+	CLD
 LOC1:
-	MOV AL, [SI]
-	MOV [DI], AL
-	INC SI
-	INC DI
+	LODSB
+	STOSB
 	DEC CX
 	JNZ LOC1
 
 	MOV AH, 4CH
-	INT 21H
-CODE ENDS
+        INT 21H
+ENDS
 END START
